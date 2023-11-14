1aed674beb46effa013771ab3e61b38ef2fa8a39
ANK <kalupahanaadheetha5@gmail.com>
2023-11-15 00:28:56 +0530

    add food pane and navigation to header donut and coffee

--- controller/OrderController.js
+++ controller/OrderController.js
@@ -7,3 +7,14 @@ $('#my-list').on('click',()=>{
     const customer_id = document.getElementById("customer-id-h3");
     customer_id.innerHTML = value.text();
 })
+
+$('#coffee-radio').on('click',()=>{
+    console.log("works! coffee:)")
+    $('#image-item-type').css("background-image","url(\"/assets/img/latte-art.png\")")
+        .css("background-size","100% 100%")
+})
+$('#donut-radio').on('click',()=>{
+    console.log("works! coffee:)")
+    $('#image-item-type').css("background-image","url(\"/assets/img/donut.png\")")
+        .css("background-size","100% 100%")
+})
