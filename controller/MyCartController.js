3fcf259a2b113d9681bf9c6c87e5ab0aad12a53f
ANK <kalupahanaadheetha5@gmail.com>
2023-11-16 22:37:29 +0530

    update add to cart ; there is an issue when it comes to adding qty

--- controller/MyCartController.js
+++ controller/MyCartController.js
@@ -34,15 +34,21 @@ $("body").on('click',"#all-coffee-items>.row>.col-sm",()=>{
             cart_db.push(item);
         }
     })
-    print_cart();
+    print_cart(selected_item);
 })
 
-const print_cart = ()=>{
+const print_cart = (selected_item)=>{
 
-    $('#cart > ol').empty();
+    $("#cart > ol").empty();
+
     for (var i = 0; i < cart_db.length; i++) {
+        //get all the lines in cart ol
+        const lineCollection = document.querySelectorAll("#cart > ol >li");
+        const lineArray = Array.from(lineCollection);
 
-        const line = `<li class="list-group-item d-flex justify-content-between align-items-start">
+        if(lineArray.length == 0){
+            //if the cart is empty
+            const line = `<li class="list-group-item d-flex justify-content-between align-items-start">
                             <div class="ms-2 me-auto">
                                 <div class="item-number">
                                     <p>${i}</p>
@@ -60,7 +66,45 @@ const print_cart = ()=>{
                             </div>
                         </li>`;
 
-        console.log(cart_db[i].item_code);
-        $('#cart > ol').append(line);
+            $('#cart > ol').append(line);
+        }else {
+            //if the cart list is not empty
+
+            for (let i=0;i<lineArray.length;i++){
+                //check if this item already exists
+                let code = lineArray[i].querySelector("div > h5").innerText;
+                if(selected_item == code){
+                    let qty = lineArray[i].querySelector("span").innerText;
+                    qty++;
+                    lineArray[i].querySelector("span").innerText = qty;
+                    break;
+                }else {
+                    //if the item already does not exist
+                    if (code != cart_db[i].item_code){
+                        const line = `<li class="list-group-item d-flex justify-content-between align-items-start">
+                            <div class="ms-2 me-auto">
+                                <div class="item-number">
+                                    <p>${i}</p>
+                                </div>
+                                <div class="fw-bold"></div>
+                                <h5>${cart_db[i].item_code}</h5>
+                                <p >${cart_db[i].item_name}</p>
+                            </div>
+                            <div class="price">
+                                <p>${cart_db[i].item_price}</p>
+                            </div>
+                            <span class="badge bg-primary rounded-pill">${1}</span>
+                            <div class="delete">
+                                <img src="/assets/img/delete%20(2).png" alt="delete-img" width="40px" height="40px">
+                            </div>
+                        </li>`;
+
+                        $('#cart > ol').append(line);
+                    }
+                }
+
+            }
+        }
     }
 }
+
